63975ca021d14c223fa92e6c478218cf9677dca4
Benhur-Linux <benhurbbs@gmail.com>
2019-11-04 22:20:29 -0300

    Correção de Dir p dir

--- aprendendo/dir_help.py
+++ aprendendo/dir_help.py
@@ -1,6 +1,7 @@
 """
 Utilitáros Python par ate auxiliar na programação
-Dir->
+dir-> Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de dado ou variável.
 
-Help->
+Help-> Apresenta a documentação/como utilizar os atributos/propriedades e funções/métodos disponíveis
+para determinado tipo de dado ou variável.
 """

6883089fec29ab70b3ed14e894692614a4def9ee
Benhur-Linux <benhurbbs@gmail.com>
2019-11-04 23:18:27 -0300

    Alterando código do print idade

--- aprendendo/recebendo_dados_usuario.py
+++ aprendendo/recebendo_dados_usuario.py
@@ -6,10 +6,15 @@ Recebendo dados de usuários.
 print('Qual o seu nome? ')
 nome = input()
 
+print('Seja bem vindo(a), {0}.'.format(nome))
+
 print('Qual a sua idade? ')
 idade = input()
 
 # Processamento
 
 # Saída de Dados
-print('Seja bem vindo(a) %s' %  (nome,idade))
+
+#Exemplo de print antigo
+#print('Você, %s, tem %s anos.' %  (nome,idade))
+print('Você, {0}, tem {1} anos.'.format(nome,idade))

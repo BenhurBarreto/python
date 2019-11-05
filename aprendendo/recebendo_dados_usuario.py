600cc17f58ffc6137ffad832f52a8a4946b0b96e
Benhur-Linux <benhurbbs@gmail.com>
2019-11-04 22:57:00 -0300

    Inserindo saida de dados

--- aprendendo/recebendo_dados_usuario.py
+++ aprendendo/recebendo_dados_usuario.py
@@ -3,9 +3,10 @@ Recebendo dados de usuários.
 """
 
 # Entrada de Dados
-print("Qual o seu nome? ")
+print('Qual o seu nome? ')
 nome = input()
 
 # Processamento
 
 # Saída de Dados
+print('Seja bem vindo(a) %s' %  nome)
